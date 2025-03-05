74554f2e21b9876c5dc88c5b2339f7965d544569
prashant <prashantwaghmode05@gmail.com>
2025-03-05 17:59:54 +0530

    first commit

---
+++ src/main/java/com/spark/echoshool/EchoschoolApplication.java
@@ -0,0 +1,13 @@
+package com.spark.echoshool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spark/echoshool/EchoschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.echoshool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
